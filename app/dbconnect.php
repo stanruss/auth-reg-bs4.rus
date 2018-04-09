b60fee8e6c07d383c56cdebebda9f5f494b9e06b
Stanruss <stan19781@gmail.com>
2018-04-09 13:00:11 +0300

    4

--- app/dbconnect.php
+++ app/dbconnect.php
@@ -3,9 +3,9 @@
     header('Content-Type: text/html; charset=utf-8');
 
     $server = "localhost"; /* имя хоста (уточняется у провайдера), если работаем на локальном сервере, то указываем localhost */
-    $username = "u0343395_stanrus"; /* u0343395_stanrus */
-    $password = "stan818791"; /* stan818791 */
-    $database = "u0343395_u034339"; /* u0343395_u034339 */
+    $username = "root"; /* u0343395_stanrus */
+    $password = "818791"; /* stan818791 */
+    $database = "auth-reg"; /* u0343395_u034339 */
  
     // Подключение к базе данных через MySQLi
     $mysqli = new mysqli($server, $username, $password, $database);
@@ -20,7 +20,7 @@
     $mysqli->set_charset('utf8');
 
     //Для удобства, добавим здесь переменную, которая будет содержать название нашего сайта
-    $address_site = "http://poecilotheria.ru/app/";
+    $address_site = "/app/";
 
     //Почтовый адрес администратора сайта
     $email_admin = "stan19781@gmail.com";
--- app/header.php
+++ app/header.php
@@ -1,66 +1,88 @@
 <?php 
-    //Запускаем сессию
-    session_start();
+	//Запускаем сессию
+session_start();
 ?>
 <!DOCTYPE html>
 <html lang="ru">
 
 <head>
 
-    <meta charset="utf-8">
-    <!-- <base href="/"> -->
+	<meta charset="utf-8">
+	<!-- <base href="/"> -->
 
-    <title>OptimizedHTML 4</title>
-    <meta name="description" content="">
+	<title>OptimizedHTML 4</title>
+	<meta name="description" content="">
 
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-    
-    <!-- Template Basic Images Start -->
-    <meta property="og:image" content="path/to/image.jpg">
-    <link rel="icon" href="app/img/favicon/favicon.ico">
-    <link rel="apple-touch-icon" sizes="180x180" href="app/img/favicon/apple-touch-icon-180x180.png">
-    <!-- Template Basic Images End -->
-    
-    <!-- Custom Browsers Color Start -->
-    <meta name="theme-color" content="#000">
-    <!-- Custom Browsers Color End -->
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	
+	<!-- Template Basic Images Start -->
+	<meta property="og:image" content="path/to/image.jpg">
+	<link rel="icon" href="app/img/favicon/favicon.ico">
+	<link rel="apple-touch-icon" sizes="180x180" href="app/img/favicon/apple-touch-icon-180x180.png">
+	<!-- Template Basic Images End -->
+	
+	<!-- Custom Browsers Color Start -->
+	<meta name="theme-color" content="#000">
+	<!-- Custom Browsers Color End -->
 
-    <link rel="stylesheet" href="app/css/main.min.css">
+	<link rel="stylesheet" href="app/css/main.min.css">
 
 </head>
 
 <body class="text-dark">
-    <header>
-        <nav class="navbar navbar-expand-md navbar-dark bg-dark">
-            <a class="navbar-brand" href="/">Navbar</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+	<header>
+		<nav class="navbar navbar-expand-md navbar-dark bg-dark">
+			<a class="navbar-brand" href="/">Navbar</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
 
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <div class="navbar-nav ml-auto">
-                    <?php 
-        //Проверяем, авторизован ли пользователь
-                    if(!isset($_SESSION['email']) && !isset($_SESSION['password'])){
-        // если нет, то выводим блок со ссылками на страницу регистрации и авторизации
-                        ?>
-                        <div class="nav-item">
-                            <a class="nav-link" href="app/form_register.php">Регистрация </a>
-                        </div>
-                        <div class="nav-item">
-                            <a class="nav-link" href="app/form_auth.php">Авторизация</a>
-                        </div>
-                        <?php
-                    }else{
-    //Если пользователь авторизован, то выводим ссылку Выход
-                        ?>  
-                        <div id="link_logout">
-                            <a href="app/logout.php">Выход</a>
-                        </div>
-                        <?php
-                    }
-                    ?>
-                </div>
-            </nav>
-        </header>
+			<div class="collapse navbar-collapse" id="navbarSupportedContent">
+				<div class="navbar-nav ml-auto">
+					<?php 
+		//Проверяем, авторизован ли пользователь
+					if(!isset($_SESSION['email']) && !isset($_SESSION['password'])){
+		// если нет, то выводим блок со ссылками на страницу регистрации и авторизации
+						?>
+						<div class="nav-item">
+							<a class="nav-link" href="app/form_register.php">Регистрация </a>
+						</div>
+						<div class="nav-item">
+							<a class="nav-link" href="app/form_auth.php">Авторизация</a>
+						</div>
+						<?php
+					}else{
+	//Если пользователь авторизован, то выводим ссылку Выход
+						?>  
+						<div id="link_logout">
+							<a href="app/logout.php">Выход</a>
+						</div>
+						<?php
+					}
+					?>
+				</div>
+			</nav>
+		</header>
+		<?php 
+		//Проверяем, авторизован ли пользователь
+		if(!isset($_SESSION['email']) && !isset($_SESSION['password'])){
+		// если нет, то выводим блок со ссылками на страницу регистрации и авторизации
+			?>
+			<div class="container">
+				<div class="row col">
+не зарегестрирован
+				</div>
+			</div>	
+			<?php
+					}else{
+	//Если пользователь авторизован, то выводим ссылку Выход
+						?>  
+						<div class="container">
+				<div class="row col">
+зарегестрирован
+				</div>
+			</div>	
+						<?php
+					}
+					?>		
